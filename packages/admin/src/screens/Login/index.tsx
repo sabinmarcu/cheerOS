78598504826a809a06e73160d4ccdd9c3fa0b95a
Sabin Marcu <sabinmarcu@gmail.com>
2020-04-11 19:47:59 +0300

    feat: add server feedback on login

--- packages/admin/src/screens/Login/index.tsx
+++ packages/admin/src/screens/Login/index.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useCallback } from 'react';
+import React, { useState, useMemo, useCallback } from 'react';
 
 import {
   useHistory,
@@ -15,7 +15,11 @@ import {
 import {
   Button,
   CardContent,
+  Snackbar,
+  IconButton,
 } from '@material-ui/core';
+
+import CloseIcon from '@material-ui/icons/Close';
 
 import {
   StyledCard,
@@ -33,7 +37,7 @@ import {
   useAdminLogin,
 } from '@cheeros/stores';
 
-const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 const minLengthValidator = (len: number): ValidatorType =>
   (value) => {
     if (value.length < len) {
@@ -81,55 +85,80 @@ export const LoginScreen: React.FC = () => {
     [email, password]
   );
   const [login, isLoginReady] = useAdminLogin();
+  const [error, setError] = useState<string | null>(null);
+  const closeError = useCallback(
+    () => setError(null),
+    [setError],
+  );
 
   const loginHandler = useCallback(
     () => {
       if (login) {
+        setError(null);
         login(email.value, password.value)
           .then(() => history.push(routes.root.route))
+          .catch(({ message }: { message: string }) => setError(message))
       }
     },
-    [login, email.value, password.value, history],
+    [login, email.value, password.value, history, setError],
   );
   return (
-    <CenterLayout>
-      <StyledCard>
-        <CardContent>
-          <StyledTypography variant="h2">Login:</StyledTypography>
-          <StyledTextField
-            required
-            fullWidth
-            value={email.value}
-            error={email.isDirty && !email.isValid}
-            helperText={email.isDirty && email.errors.length > 0 ? email.errors[0] : undefined}
-            onChange={email.handler}
-            placeholder="leroy@jenkins.com"
-            type="email"
-          />
-          <StyledTextField
-            required
-            fullWidth
-            value={password.value}
-            error={password.isDirty && !password.isValid}
-            helperText={password.isDirty && password.errors.length > 0 ? password.errors[0] : undefined}
-            onChange={password.handler}
-            placeholder="test1234"
-            type="password"
-          />
-        </CardContent>
-        <StyledCardActions>
-          <Button
-            color="primary"
-            variant="contained"
-            onClick={loginHandler}
-            disabled={!isValid || !isLoginReady}
-          >
-            Login!
-          </Button>
-          {isDirty && <Button color="secondary" onClick={reset}>Reset</Button>}
-        </StyledCardActions>
-      </StyledCard>
-    </CenterLayout>
+    <>
+      <CenterLayout>
+        <StyledCard>
+          <CardContent>
+            <StyledTypography variant="h2">Login:</StyledTypography>
+            <StyledTextField
+              required
+              fullWidth
+              value={email.value}
+              error={email.isDirty && !email.isValid}
+              helperText={email.isDirty && email.errors.length > 0 ? email.errors[0] : undefined}
+              onChange={email.handler}
+              placeholder="leroy@jenkins.com"
+              type="email"
+            />
+            <StyledTextField
+              required
+              fullWidth
+              value={password.value}
+              error={password.isDirty && !password.isValid}
+              helperText={password.isDirty && password.errors.length > 0 ? password.errors[0] : undefined}
+              onChange={password.handler}
+              placeholder="test1234"
+              type="password"
+            />
+          </CardContent>
+          <StyledCardActions>
+            <Button
+              color="primary"
+              variant="contained"
+              onClick={loginHandler}
+              disabled={!isValid || !isLoginReady}
+            >
+              Login!
+            </Button>
+            {isDirty && <Button color="secondary" onClick={reset}>Reset</Button>}
+          </StyledCardActions>
+        </StyledCard>
+      </CenterLayout>
+
+      <Snackbar
+        open={!!error}
+        autoHideDuration={6000}
+        onClose={closeError}
+        message={error}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        action={
+          <IconButton onClick={closeError}>
+            <CloseIcon style={{ color: 'white' }} />
+          </IconButton>
+        }
+      />
+    </>
   )
 };
 
